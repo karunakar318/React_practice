f05cff131ba3522d58de0b805af88ed6c68d8bbf
karunakar <karunakarnarlakonda@gmail.com>
2023-12-07 16:54:49 +0530

    Changed APP.js

--- App.js
+++ App.js
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client"; 
 let h1=React.createElement("h1",{},"namste ALL");
-let h2=React.createElement("h2",{},"namste EveryOne");
+let h2=React.createElement("h2",{},"I'm Karunakar");
 let d=React.createElement("div",{},[h1,h2]);
-let r=ReactDOM.createRoot(document.getElementById('root'));
-r.render(d);
+let render=ReactDOM.createRoot(document.getElementById('root'));
+render.render(d);
