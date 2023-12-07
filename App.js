c2842e8434b0d47ecfb2ba639f81662693670137
karunakar318 <96810607+karunakar318@users.noreply.github.com>
2023-12-07 18:50:19 +0530

    Update App.js

--- App.js
+++ App.js
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client"; 
 let h1=React.createElement("h1",{},"namste ALL");
-let h2=React.createElement("h2",{},"namste EveryOne");
+let h2=React.createElement("h2",{},"I'm karunakar");
 let d=React.createElement("div",{},[h1,h2]);
 let r=ReactDOM.createRoot(document.getElementById('root'));
 r.render(d);
