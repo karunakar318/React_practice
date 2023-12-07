796a02ec8325040e252660606e21200ccf63a024
karunakar <karunakarnarlakonda@gmail.com>
2023-12-07 13:40:51 +0530

    first commit

--- App.js
+++ App.js
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client"; 
-let h1=React.createElement("h1",{},"namste");
-let h2=React.createElement("h2",{},"namste");
+let h1=React.createElement("h1",{},"namste ALL");
+let h2=React.createElement("h2",{},"namste EveryOne");
 let d=React.createElement("div",{},[h1,h2]);
 let r=ReactDOM.createRoot(document.getElementById('root'));
 r.render(d);
